682b3d966d852774f29569f9f44fad7409d26c41
navi-punk <99031334+navi-punk@users.noreply.github.com>
2022-02-05 09:58:08 -0500

    Update ajax.js

--- ajax/ajax.js
+++ ajax/ajax.js
@@ -21,4 +21,35 @@
             $("#records_content").html(data);
         });
     }
+        
+   // Add Record
+function addRecord() {
+
+    var variable1 = $("#variable1").val();
+    var variable2 = $("#variable2").val();
+    var operador = $("#operador").val();
+    var resp = $("#resp").val();
+
+
+    // Add record
+    $.post("ajax/agregarDatos.php", {
+        variable1: variable1,
+        variable2: variable2,
+        operador: operador,
+        resp:resp
+    }, function (data, status) {
+        // close the popup
+        $("#add_new_record_modal").modal("hide");
+
+        // read records again
+        readRecords();
+
+        // clear fields from the popup
+        $("#variable1").val("");
+        $("#variable2").val("");
+        $("#operador").val("");
+        $("#resp").val("");
+
+    });
+}
     });
