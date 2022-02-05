74a456c8d98c3f9bbbce17029e4a7e5f83c24da1
navi-punk <99031334+navi-punk@users.noreply.github.com>
2022-02-05 02:31:35 -0500

    Update conection.php

--- ajax/conection.php
+++ ajax/conection.php
@@ -6,14 +6,12 @@ $password = "b79185f9e9b7239bf5bbaee373cbe5e21e1408681a6b79b8959144f8d333f904";
 $port = "5432";
 $database = "dkl0mam6p80g2";
 
+echo "Archivo de conexion";
+//$con = new pg_connect($host, $user, $password, $database,$port);
 
-$con = new pg_connect($host, $user, $password, $database);
+$dbconn = pg_connect("host=ec2-54-157-15-228.compute-1.amazonaws.com dbname=dkl0mam6p80g2 user=lrktyntpbdnlhi password=b79185f9e9b7239bf5bbaee373cbe5e21e1408681a6b79b8959144f8d333f904")
+    or die('Could not connect: ' . pg_last_error());
 
 
-if ($con->connect_error) {
-    die("Connection failed: " . $con->connect_error);
-}else{
-    echo "conecto";
-}
 
 ?>
