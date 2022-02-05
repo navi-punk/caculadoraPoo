2996233612941744f7b1470b799d7e066a1e7acb
navi-punk <99031334+navi-punk@users.noreply.github.com>
2022-02-05 17:35:05 -0500

    Update agregarDatos.php

--- ajax/agregarDatos.php
+++ ajax/agregarDatos.php
@@ -1,23 +1,14 @@
 <?php
-	
-		include("conection.php");
-		echo "INSERT";
-		// get values
+include("conection.php");
 
-		$variable1 = $_POST['variable1'];
-		$variable2 = $_POST['variable2'];
-    		$operador = $_POST['operador'];
-	    	$resp = $_POST['resp'];
-
+function insertar($variable1,$variable2,$operador,$resp){
+	 $query = "INSERT INTO calculadora.operaciones(numero1, numero2, operador, resultado) VALUES('$variable1', '$variable2', '$operador', '$resp')";
+	 $result = pg_query($dbconn, $query);
 		
-		$query = "INSERT INTO calculadora.operaciones(numero1, numero2, operador, resultado) VALUES(1, 3, 'mult', 3)";
-		$result = pg_query($dbconn, $query);
-		
-		if ($result === false) {
-		    print pg_last_error($dbconn);
-		} else {
-		    print 'everything was ok';
-		}
-
-		//$query = "INSERT INTO operaciones(numero1, numero2, operador, resultado) VALUES('$variable1', '$variable2', '$operador', '$resp', '$date')";
-		?>
+	if ($result === false) {
+		print pg_last_error($dbconn);
+	} else {
+		print 'everything was ok';
+	}
+}
+?>
