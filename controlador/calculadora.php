70212ef5b7a37152d22e594543d4653f8a2d4d4e
navi-punk <99031334+navi-punk@users.noreply.github.com>
2022-02-05 11:05:14 -0500

    Update calculadora.php

--- controlador/calculadora.php
+++ controlador/calculadora.php
@@ -1,4 +1,21 @@
 <?php
+    include("conection.php");
+
+		// get values
+
+		$variable1 = $_POST['variable1'];
+		$variable2 = $_POST['variable2'];
+        $operador = $_POST['operador'];
+        $resp = $_POST['resp'];
+
+
+
+		$query = "INSERT INTO operaciones(numero1, numero2, operador, resultado) VALUES('$variable1', '$variable2', '$operador', '$resp', '$date')";
+		if (!$result = pg_query($dbconn, $query)) {
+	        exit(pg_error($dbconn));
+	    }
+	    echo "1 Record Added!";
+
     //captura de datos 
     $variable1 = $_POST["variable1"];
     $variable2 = $_POST["variable2"];
