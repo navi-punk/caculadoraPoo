1c8ae004864382b17c339e6a12f42d0669de47e0
navi-punk <99031334+navi-punk@users.noreply.github.com>
2022-02-05 10:30:56 -0500

    Create agregarDatos.php

---
+++ ajax/agregarDatos.php
@@ -0,0 +1,22 @@
+<?php
+	if(isset($_POST['resp']))
+	{
+
+		include("conexion.php");
+
+		// get values
+
+		$variable1 = $_POST['variable1'];
+		$variable2 = $_POST['variable2'];
+    $operador = $_POST['operador'];
+    $resp = $_POST['resp'];
+
+
+
+		$query = "INSERT INTO operaciones(numero1, numero2, operador, resultado) VALUES('$variable1', '$variable2', '$operador', '$resp', '$date')";
+		if (!$result = pg_query($con, $query)) {
+	        exit(pg_error($con));
+	    }
+	    echo "1 Record Added!";
+	}
+?>
