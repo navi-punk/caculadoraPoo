d4eabc89e1934804c3887ae22732cf010dc96f29
navi-punk <99031334+navi-punk@users.noreply.github.com>
2022-02-05 18:27:15 -0500

    Update calculadora.php

--- ajax/calculadora.php
+++ ajax/calculadora.php
@@ -66,14 +66,5 @@
     
     }
 
-	function insertar(){
-		 $query = "INSERT INTO calculadora.operaciones(numero1, numero2, operador, resultado) VALUES(3, 2, 'suma', 5)";
-		 $result = pg_query($dbconn, $query);
-	         if ($result === false) {
-			$resultado = pg_last_error($dbconn);
-		} else {
-			$resultado = 'everything was ok';
-		}		
-		 //echo $resultado;	
-	}
+
 ?>
